b0399279e1e94203554f72f3211b69f6704fb4e4
viktor <viktordojnov@hotmail.com>
2020-04-29 14:22:15 -0700

    create test table in db

--- models.py
+++ models.py
@@ -19,3 +19,8 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
+
+class Venues(db.Model):
+    __tablename__ = 'venues'
+
+    id = Column(Integer, primary_key=True)
