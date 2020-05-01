8d133242aecf9818efdf2a8eff338d5de5a430cf
viktor <viktordojnov@hotmail.com>
2020-04-30 23:10:32 -0700

    Add models for app
    Add Restaurant, Reservation, and Menu Item models

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 
-from sqlalchemy import Column, String, create_engine, Integer
+from sqlalchemy import Column, String, create_engine, Integer, DateTime, Numeric
 from flask_sqlalchemy import SQLAlchemy
 import json
 import os
@@ -19,9 +19,35 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
-
-class Venues(db.Model):
-    __tablename__ = 'venues'
+# Table to store information about the restaurants
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
+    address = Column(String)
+    owner_id = Column(String)  # owner_id will be retrieved from token to see which user owns the restaurant
+
+# Table to store information about the reservations at restaurants
+class Reservations(db.Model):
+    __tablename__ = 'reservations'
+
+    id = Column(Integer, primary_key=True)
+    time_of_res = Column(DateTime)
+    num_of_people = Column(Integer)
+    name_for_res = Column(String) 
+    customer_id = Column(String) # customer_id will be retrieved from token
+    restaurant_id = Column(Integer)
+
+# Table to store information about the Menu Items at restaurants
+class MenuItems(db.Model):
+    __tablename__ = 'menu_items'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    price = Column(Numeric)
+    restaurant_id = Column(Integer)
+
+
+
