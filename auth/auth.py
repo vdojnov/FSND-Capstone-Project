a4cd993ba9940a1e2ba1eba30480fef9ce7ca228
viktor <viktordojnov@hotmail.com>
2020-05-09 12:43:30 -0700

    Update auth.py

--- auth/auth.py
+++ auth/auth.py
@@ -3,6 +3,7 @@ from flask import request, _request_ctx_stack, abort
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
+import os
 
 
 # Change this with new Auth0 info
