906809553118626d5145a11ccb9b92a6fcaa5662
viktor <viktordojnov@hotmail.com>
2020-05-09 13:04:32 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
-$env:DATABASE_URL="postgresql://postgres:viktor@localhost:5432/fsndcapstone"
-$env:FLASK_APP="app.py"
-$env:FLASK_ENV="development"   
+# $env:DATABASE_URL="postgresql://postgres:viktor@localhost:5432/fsndcapstone"
+# $env:FLASK_APP="app.py"
+# $env:FLASK_ENV="development"   
+# $env:AUTH0_DOMAIN='fsnd-happyhour.auth0.com'
+# $env:ALGORITHMS="['RS256']"
+# $env:API_AUDIENCE='fsndcapstone'
 
 DATABASE_URL
 AUTH0_DOMAIN
 ALGORITHMS
 API_AUDIENCE
+
