27dd68a56c5d451cda42f33b898ea69ba3e74841
viktor <viktordojnov@hotmail.com>
2020-05-09 12:38:24 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 $env:DATABASE_URL="postgresql://postgres:viktor@localhost:5432/fsndcapstone"
 $env:FLASK_APP="app.py"
 $env:FLASK_ENV="development"   
+
+DATABASE_URL
+AUTH0_DOMAIN
+ALGORITHMS
+API_AUDIENCE
